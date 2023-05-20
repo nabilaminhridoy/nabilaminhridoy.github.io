b91070736fcc1fea3204951ba1bf90f885c11ef8
Nabil Amin Hridoy <66374353+nabilaminhridoy@users.noreply.github.com>
2023-05-21 02:52:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "UI Designer", "Data Analysist", "Freelancer"],
+        strings: ["Web Designer", "UI & UX Designer", "Freelancer"],
         typeSpeed: 60,
         backSpeed: 30,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "UI Designer", "Data Analysist", "Freelancer"],
+        strings: ["Web Designer", "UI & UX Designer", "Freelancer"],
         typeSpeed: 60,
         backSpeed: 30,
         loop: true
